dbe02bf55619e25d80338cf78dd936b4d40bad62
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2020-06-04 21:36:00 +0300

    oopsie

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const testedLength = 6;
 module.exports = function (mod) {
 	mod.hook("S_CHAT", 3, event => {
 		let messageWithoutTags = event.message.replace("<FONT>", "");
-		let messageWithoutTags = messageWithoutTags.replace("</FONT>", "");
+		messageWithoutTags = messageWithoutTags.replace("</FONT>", "");
 
 		let strForTest = messageWithoutTags.length > testedLength ? messageWithoutTags.substring(0,testedLength) : messageWithoutTags;
 		if(cyrillicPattern.test(strForTest)) return false;
